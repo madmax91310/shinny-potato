c2252855cdecb320133c811b08a254194f9d14fe
madmax91310 <54199744+madmax91310@users.noreply.github.com>
2026-09-23 21:54:54 +0200

    Register concrete investing cases

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import TweetMidi from './pages/tweet-midi/App'
 import IndexComparator from './pages/index-comparator/App'
 import FeeImpact from './pages/fee-impact/App'
 import MarketFacts from './pages/market-facts/App'
+import ConcreteCases from './pages/concrete-cases/App'
 import TweetBank from './pages/tweet-bank/App'
 import { TOOLS } from './tools'
 
@@ -31,6 +32,7 @@ const TOOL_ELEMENTS = {
   '/comparateur-indices': <IndexComparator />,
   '/impact-frais': <FeeImpact />,
   '/faits-marquants-marches': <MarketFacts />,
+  '/cas-concrets': <ConcreteCases />,
   '/banque-tweets': <TweetBank />,
 }
 
